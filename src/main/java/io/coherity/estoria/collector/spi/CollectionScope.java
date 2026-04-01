ef68e7766dd18704fec42df7ac2453cbe052a197
kapil dhingra <kapil.dhingra@coherity.io>
2026-04-01 15:08:32 -0500

    removed scope dimensions

--- src/main/java/io/coherity/estoria/collector/spi/CollectionScope.java
+++ src/main/java/io/coherity/estoria/collector/spi/CollectionScope.java
@@ -1,7 +1,7 @@
 package io.coherity.estoria.collector.spi;
 
 import java.util.Map;
-import java.util.Optional;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,10 +10,10 @@ import lombok.Data;
 public class CollectionScope
 {
     private CloudProvider provider;
-    private Map<ScopeDimension, String> dimensions;
     private Map<String, String> attributes;
-    public Optional<String> getValue(ScopeDimension dimension)
-    {
-        return Optional.ofNullable(dimensions.get(dimension));
-    }
+//    private Map<ScopeDimension, String> dimensions;
+//    public Optional<String> getValue(ScopeDimension dimension)
+//    {
+//        return Optional.ofNullable(dimensions.get(dimension));
+//    }
 }
