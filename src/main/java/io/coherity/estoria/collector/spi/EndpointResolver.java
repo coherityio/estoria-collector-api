82a4ad45c3296238498b4be87204f1ef717b7f0a
kapil dhingra <kapil.dhingra@coherity.io>
2026-04-10 20:17:20 -0500

    cleanup

--- src/main/java/io/coherity/estoria/collector/spi/EndpointResolver.java
+++
@@ -1,8 +0,0 @@
-package io.coherity.estoria.collector.spi;
-
-import java.util.Optional;
-
-public interface EndpointResolver
-{
-    Optional<EndpointReference> resolve(String serviceId, ProviderContext providerContext);
-}
--- src/main/java/io/coherity/estoria/collector/spi/ProviderSession.java
+++
@@ -1,8 +0,0 @@
-package io.coherity.estoria.collector.spi;
-
-public interface ProviderSession extends AutoCloseable
-{
-    CloudProvider getProvider();
-    EndpointResolver getEndpointResolver();
-    <T> T getService(Class<T> serviceType) throws ProviderException;
-}
